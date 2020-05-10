7b27693ec12e98a266fb3363a89d6f5ff0afcf62
zq <164025309@qq.com>
2020-05-10 23:33:46 +0800

    11

--- src/com/neusoft/order/Dao/OrderDao.java
+++ src/com/neusoft/order/Dao/OrderDao.java
@@ -6,7 +6,6 @@ import com.neusoft.order.entity.Order;
 public interface OrderDao {
 	//借阅书籍--添加
 	int add(Order o);
-	
 	//查询订单--用户查自己的订单，如果是管理员查的话，直接select *即可
 	public List<Order> showOrder(int stu_id);
 	//归还书籍
